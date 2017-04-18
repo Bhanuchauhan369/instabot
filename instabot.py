c7d526fdc6915d681ae1b2685007a20e256f9cce
Bhanuchauhan369 <bhanuchauhan369@gmail.com>
2017-04-18 10:33:07 -0700

    like and comment on user post

--- instabot.py
+++ instabot.py
@@ -11,25 +11,51 @@ def self_info():
     my_info = requests.get(url).json()
     print (my_info)
     print(my_info['data']['id'])
-self_info()
+#self_info()
 
 
 # user information display function
 def user_info(insta_user):
     url= base_url+'users/search?q='+insta_user +'&access_token='+ACCESS_TOKEN
     user_info = requests.get(url).json()
-    print (user_info)
-    print(user_info['data'][0]['username'])
-    print(user_info['meta']['code'])
+    #print (user_info)
+    #print(user_info['data'][0]['username'])
+    #print(user_info['meta']['code'])
 
     print(user_info['data'][0]['id'])
     return user_info['data'][0]['id']
-user_info("ananya_thakur333")
+#user_info("ananya_thakur333")
 
 #  get user post
 def get_user_post(insta_username):
     insta_user_id=user_info(insta_username)
     request_url=base_url +'users/'+str(insta_user_id)+'/media/recent/?access_token='+ACCESS_TOKEN
     recent_post = requests.get(request_url).json()
+    print("user's recent post is:"+str(recent_post['data'][0]['link']))
     print (recent_post)
-get_user_post("ananya_thakur333")
+    return recent_post['data'][0]['id']
+
+#get_user_post("ananya_thakur333")
+
+#fuction to like the user post
+def like_post(insta_user):
+    id = get_user_post(insta_user)
+    payload = {"access_token": ACCESS_TOKEN}
+    req_url = base_url + "media/" + id + "/likes"
+    like_response = requests.post(req_url, payload).json()
+    print("op is:::")
+    print(like_response)
+#like_post("ananya_thakur333")
+
+
+# comment on user post..
+def comment_on_user_post(insta_user):
+    id=get_user_post(insta_user)
+    request_url = base_url + 'media/' + id + '/comments?access_token=' + ACCESS_TOKEN
+    payload = {"access_token": ACCESS_TOKEN, 'text': 'hello you'}
+    comment_response = requests.post(request_url, payload).json()
+    print("op is:")
+    print(comment_response)
+
+
+comment_on_user_post("ananya_thakur333")
